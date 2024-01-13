bbd1da3fcc9b8c7db6c1b8bed166127c6a877916
Efraim Munthe <efrayanglain@gmail.com>
2024-01-13 14:46:55 +0700

    setup database

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module fiber-student-api
 
 go 1.21.5
 
-require github.com/gofiber/fiber/v2 v2.52.0
+require (
+	github.com/gofiber/fiber/v2 v2.52.0
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+)
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,40 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+var DB *sql.DB
+
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Failed to load env file")
+	}
+
 	app := fiber.New()
+
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbname := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", user, password, host, port, dbname)
+	DB, err = sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("successfully connected to database")
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("halo")
